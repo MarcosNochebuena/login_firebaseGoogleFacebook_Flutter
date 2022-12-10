320fe398e835fdd0ec3b73a8045279651d6dcf08
MarcosNochebuena <MARK_NOCHE@hotmail.com>
2022-12-10 09:41:42 -0600

    Login funcionando

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,94 @@
+import 'package:login_flutter/screens/login_screen.dart';
+import 'package:login_flutter/utils/next_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:login_flutter/provider/sign_in_provider.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future getData() async {
+    final sp = context.read<SignInProvider>();
+    sp.getDataFromSharedPreferences();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // change read to watch!!!!
+    final sp = context.watch<SignInProvider>();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              backgroundColor: Colors.white,
+              backgroundImage: NetworkImage("${sp.imageUrl}"),
+              radius: 50,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Bienvenido ${sp.name}",
+              style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Correo: ${sp.email}",
+              style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              "UID: ${sp.uid}",
+              style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("PROVIDER:"),
+                const SizedBox(
+                  width: 5,
+                ),
+                Text("${sp.provider}".toUpperCase(),
+                    style: const TextStyle(color: Colors.red)),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  sp.userSignOut();
+                  nextScreenReplace(context, const LoginScreen());
+                },
+                child: const Text("Cerra Sesion",
+                    style: TextStyle(
+                      color: Colors.white,
+                    )))
+          ],
+        ),
+      ),
+    );
+  }
+}
